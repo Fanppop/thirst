d8eacc3efb3fd9f38ec84a27afef7ff5838f3326
Yarick <fanppop17@gmail.com>
2023-05-21 18:33:32 +1000

    4

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,55 @@
+import React from 'react'
 import './App.css'
 
-function App() {
-  const item = {
-    brand: 'Tiger of Sweden',
-    title: 'Leonard coat',
-    description: 'Minimalistic coat in cotton-blend',
-    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-    price: 399,
-    currency: '£'
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.item = {
+      brand: 'Tiger of Sweden',
+      title: 'Leonard coat',
+      description: 'Minimalistic coat in cotton-blend',
+      descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+      price: 399,
+      currency: '£'
+    }
   }
 
-  const ShopItemFunc = () => {
+  ShopItemFunc = () => {
     return (
       <div className="main-content">
-        <h2>{item.brand}</h2>
-        <h1>{item.title}</h1>
-        <h3>{item.description}</h3>
+        <h2>{this.item.brand}</h2>
+        <h1>{this.item.title}</h1>
+        <h3>{this.item.description}</h3>
         <div className="description">
-          {item.descriptionFull}
+          {this.item.descriptionFull}
         </div>
         <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
         <div className="divider"></div>
         <div className="purchase-info">
-          <div className="price">{item.currency}{item.price},00</div>
+          <div className="price">{this.item.currency}{this.item.price},00</div>
           <button>Добавить в корзину</button>
         </div>
       </div>
     )
   }
 
-  
-  // Внутри компонента App
-  return (
-    <div className="container">
-      <div className="background-element">
+  render() {
+
+    
+    // Внутри компонента App
+    return (
+      <div className="container">
+        <div className="background-element">
+        </div>
+        <div className="highlight-window">
+          <div className='highlight-overlay'></div>
+        </div>
+        <div className="window">
+          <this.ShopItemFunc/>
+        </div>
       </div>
-      <div className="highlight-window">
-        <div className='highlight-overlay'></div>
-      </div>
-      <div className="window">
-        <ShopItemFunc  item={item} />
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
